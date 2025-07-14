c7937bd0af429f97a000bff5eacab0e7bfd632f6
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-07-14 23:19:42 +0530

    feat: integrate React Query for data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer'
 import { MainLayout } from './components/layout/MainLayout'
-
+import { QueryClient , QueryClientProvider } from '@tanstack/react-query' 
 function App() {
-  return (
-    <MainLayout>
-      <div className="flex-1">
-        <Outlet />
-      </div>
-    </MainLayout>
+  const queryClient = new QueryClient()
+  return ( 
+    <QueryClientProvider client={queryClient}>
+      <MainLayout>
+        <div className="flex-1">
+          <Outlet />    
+        </div>
+      </MainLayout>
+    </QueryClientProvider>
   )
 }
 

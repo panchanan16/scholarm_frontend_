6280b87372629e13e9c16d5b23da376722682811
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-07-14 23:29:26 +0530

    feat: set up Redux store and user slice for state management; integrate store in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import './App.css'
 import Footer from './components/footer'
 import { MainLayout } from './components/layout/MainLayout'
 import { QueryClient , QueryClientProvider } from '@tanstack/react-query' 
+import store from './store/store'
+import { Provider } from 'react-redux'
 function App() {
   const queryClient = new QueryClient()
   return ( 
+    <Provider store={store}>  
     <QueryClientProvider client={queryClient}>
-      <MainLayout>
+      <MainLayout>  
         <div className="flex-1">
           <Outlet />    
         </div>
       </MainLayout>
     </QueryClientProvider>
+    </Provider>
   )
 }
 
